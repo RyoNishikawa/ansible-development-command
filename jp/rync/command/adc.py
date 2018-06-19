9343c1aa785cba300100b0acd579ad151de0d561
Ryo Nishikawa <ryo.pg.se@gmail.com>
2018-06-19 14:46:14 +0900

    Create the command.

---
+++ jp/rync/command/adc.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python3
+import sys
+
+
+def main(args=sys.argv):
+    print(args)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ test/jp/rync/command/test_adc.py
@@ -0,0 +1,17 @@
+import unittest
+
+import jp.rync.command.adc
+
+
+class TestAnsibleDevelopmentCommand(unittest.TestCase):
+
+    def setUp(self): pass
+
+    def tearDown(self): pass
+
+    def test_exec_command_without_params(self):
+        jp.rync.command.adc.main()
+
+    def test_help(self): pass
+
+    def test_create_project(self): pass
