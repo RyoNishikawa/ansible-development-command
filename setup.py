fb9c0ecf1afde4c421818398e88e4696e21a6165
Ryo Nishikawa <ryo.pg.se@gmail.com>
2018-07-09 17:38:48 +0900

    Modified version number to 0.3.0.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ansible-develop-command',
-    version='0.0.3',
+    version='0.3.0',
     author='Ryo Nishikawa',
     description='development command for ansible.',
     packages=find_packages(exclude=('test')),
