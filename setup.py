11661a8372bbf8b9b473692d41203348627be7a7
Ryo Nishikawa <ryo.pg.se@gmail.com>
2019-10-21 14:27:49 +0900

    Modified setup.py and created setup.cfg.

--- setup.py
+++ setup.py
@@ -1,15 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='ansible-development-command',
-    version='0.3.0',
-    author='Ryo Nishikawa',
-    author_email='ryo.pg.se@gmail.com',
-    url='https://github.com/RyoNishikawa/',
-    description='development command for ansible.',
-    package_dir={'': 'lib'},
-    packages=find_packages('lib'),
-    #packages=['adc'],
-    scripts=['bin/adc'],
-    test_suite='test'
-)
+setup()
