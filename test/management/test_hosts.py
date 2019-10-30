8aec4cac8db680cad6d40abaacf9454ff097b2b8
Ryo Nishikawa <ryo.pg.se@gmail.com>
2019-10-30 09:27:41 +0900

    Modified the test before and after process.

--- test/management/test_hosts.py
+++ test/management/test_hosts.py
@@ -11,13 +11,29 @@ from adc.models.hosts import InventoryFile
 class TestHosts(unittest.TestCase):
     logger = getLogger("test")
 
+    file_name_test_hosts_add_group = "test_hosts_add_group"
+    file_name_test_hosts_add_host = "test_hosts_add_host"
+    file_name_test_hosts_read_hosts = "test_hosts_read_hosts"
+    file_name_test_hosts_read_hosts_with_two_hosts = "test_hosts_read_hosts_with_two_hosts"
+
     def setUp(self):
-        # os.mkdir("tmp")
-        # os.chdir("tmp")
         # logging.config.fileConfig("../resources/logger.conf")
+        self.delete_test_files()
         pass
 
-    def tearDown(self): pass
+    def tearDown(self):
+        self.delete_test_files()
+        pass
+
+    def delete_test_files(self):
+        files = [
+            self.file_name_test_hosts_add_group,
+            self.file_name_test_hosts_add_host,
+            self.file_name_test_hosts_read_hosts,
+            self.file_name_test_hosts_read_hosts_with_two_hosts
+        ]
+        for file_name in files:
+            self._delete_file(file_name)
 
     # os.chdir("..")
     # shutil.rmtree("tmp")
@@ -26,13 +42,12 @@ class TestHosts(unittest.TestCase):
         """
         Add the gourp for hosts.
         """
-        test_file_name = "test_hosts_add_group"
-        self._create_blank_file(test_file_name)
+        self._create_blank_file(self.file_name_test_hosts_add_group)
 
-        InventoryFile(test_file_name).add_group("test_group")
+        InventoryFile(self.file_name_test_hosts_add_group).add_group("test_group")
 
-        self.assertTrue(os.path.exists("test_hosts_add_group"))
-        with open(test_file_name, 'r') as f:
+        self.assertTrue(os.path.exists(self.file_name_test_hosts_add_group))
+        with open(self.file_name_test_hosts_add_group, 'r') as f:
             result = f.readlines()
             self.logger.debug(result)
 
@@ -47,10 +62,9 @@ class TestHosts(unittest.TestCase):
         """
         Add the host.
         """
-        test_file_name = "test_hosts_add_host"
-        self._create_blank_file("test_hosts_add_host")
+        self._create_blank_file(self.file_name_test_hosts_add_host)
         #
-        InventoryFile(test_file_name).add_host(
+        InventoryFile(self.file_name_test_hosts_add_host).add_host(
             "test_group",
             "test01",
             "192.168.1.10",
@@ -59,8 +73,8 @@ class TestHosts(unittest.TestCase):
             "testuserpass"
         )
 
-        self.assertTrue(os.path.exists("test_hosts_add_host"))
-        with open(test_file_name, 'r') as f:
+        self.assertTrue(os.path.exists(self.file_name_test_hosts_add_host))
+        with open(self.file_name_test_hosts_add_host, 'r') as f:
             result = f.readlines()
             self.logger.debug(result)
 
@@ -85,9 +99,8 @@ class TestHosts(unittest.TestCase):
         """
         Read the data in the hosts file.
         """
-        test_file_name = "test_hosts_read_hosts"
-        self._create_blank_file(test_file_name)
-        with open(test_file_name, 'w') as f:
+        self._create_blank_file(self.file_name_test_hosts_read_hosts)
+        with open(self.file_name_test_hosts_read_hosts, 'w') as f:
             f.write('\n'.join([
                 "[test_group]",
                 "test01"
@@ -98,7 +111,7 @@ class TestHosts(unittest.TestCase):
                 " ansible_port=22"
             ]))
 
-        result = InventoryFile(test_file_name)
+        result = InventoryFile(self.file_name_test_hosts_read_hosts)
         result.read_hosts()
 
         self.assertTrue("test_group" in result.hosts_data)
@@ -113,48 +126,16 @@ class TestHosts(unittest.TestCase):
         """
         Read the data in the hosts file.
         """
-        test_file_name = "test_hosts_read_hosts_with_two_hosts"
-        self._create_blank_file(test_file_name)
-        with open(test_file_name, 'w') as f:
+        self._create_blank_file(self.file_name_test_hosts_read_hosts_with_two_hosts)
+        with open(self.file_name_test_hosts_read_hosts_with_two_hosts, 'w') as f:
             for line in ["[test_group]\n",
                          "test01 ansible_host=192.168.1.10 ansible_user=testuser ansible_password=testuserpass ansible_become_password=testuserpass ansible_port=22\n",
                          "[test_group2]\n",
                          "test02 ansible_host=192.168.1.11 ansible_user=testuser2 ansible_password=testuserpass2 ansible_become_password=testuserpass2 ansible_port=23"]:
                 f.write(line)
-            # f.writelines([
-            #     "[test_group]\n",
-            #     "test01 ansible_host=192.168.1.10 ansible_user=testuser ansible_password=testuserpass ansible_become_password=testuserpass ansible_port=22\n",
-            #     "[test_group2]\n",
-            #     "test02 ansible_host=192.168.1.11 ansible_user=testuser2 ansible_password=testuserpass2 ansible_become_password=testuserpass2 ansible_port=23"
-            # ])
-            # f.write('\n'.join([
-            #     "[test_group]",
-            #     "test01"
-            #     " ansible_host=192.168.1.10"
-            #     " ansible_user=testuser"
-            #     " ansible_password=testuserpass"
-            #     " ansible_become_password=testuserpass"
-            #     " ansible_port=22",
-            #     "[test_group2]",
-            #     "test02"
-            #     " ansible_host=192.168.1.11"
-            #     " ansible_user=testuser2"
-            #     " ansible_password=testuserpass2"
-            #     " ansible_become_password=testuserpass2"
-            #     " ansible_port=23"
-            # ]))
 
-        result = InventoryFile(test_file_name)
+        result = InventoryFile(self.file_name_test_hosts_read_hosts_with_two_hosts)
         result.read_hosts()
-
-        # print(result.hosts_data)
-        # print("========= result ==========")
-        # for line in result.hosts_data['test_group'].hosts:
-        #     print(line)
-        # print("========= result2 =========")
-        # for line in result.hosts_data['test_group2'].hosts:
-        #     print(line)
-        # print("===========================")
 
         self.assertTrue("test_group" in result.hosts_data)
         self.assertEqual(result.hosts_data['test_group'].hosts[0].host_name, "test01")
@@ -172,6 +153,9 @@ class TestHosts(unittest.TestCase):
         self.assertEqual(result.hosts_data['test_group2'].hosts[0].option_ansible_become_password, "testuserpass2")
         self.assertEqual(result.hosts_data['test_group2'].hosts[0].option_ansible_port, "23")
 
-    def _create_blank_file(self, file_name):
+    def _create_blank_file(self, file_name: str):
         with open(file_name, 'w') as f: pass
 
+    def _delete_file(self, file_name: str):
+        if os.path.exists(file_name):
+            os.remove(file_name)
