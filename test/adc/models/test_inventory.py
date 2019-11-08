d9a56fc70ae77f02cea044a8927fe325b1288ac8
Ryo Nishikawa <ryo.pg.se@gmail.com>
2019-11-09 07:36:09 +0900

    Added the process of to create temporary directory.

--- test/adc/models/test_inventory.py
+++ test/adc/models/test_inventory.py
@@ -1,5 +1,5 @@
 import unittest
-
+import tempfile
 import os
 
 from logging import getLogger
@@ -34,16 +34,18 @@ class TestInventory(unittest.TestCase):
         """
         Add the gourp for hosts.
         """
-        TestFile.create_blank_file(self.file_name_test_hosts_add_group)
+        with tempfile.TemporaryDirectory() as td:
+            os.chdir(td)
+            TestFile.create_blank_file(self.file_name_test_hosts_add_group)
 
-        InventoryFile(self.file_name_test_hosts_add_group).add_group("test_group")
+            InventoryFile(self.file_name_test_hosts_add_group).add_group("test_group")
 
-        self.assertTrue(os.path.exists(self.file_name_test_hosts_add_group))
-        with open(self.file_name_test_hosts_add_group, 'r') as f:
-            result = f.readlines()
-            self.logger.debug(result)
+            self.assertTrue(os.path.exists(self.file_name_test_hosts_add_group))
+            with open(self.file_name_test_hosts_add_group, 'r') as f:
+                result = f.readlines()
+                self.logger.debug(result)
 
-        self.assertEqual(result[0], "[test_group]\n")
+            self.assertEqual(result[0], "[test_group]\n")
 
     def test_delete_gourp(self):
         """
@@ -54,33 +56,34 @@ class TestInventory(unittest.TestCase):
         """
         Add the host.
         """
-        TestFile.create_blank_file(self.file_name_test_hosts_add_host)
-        #
-        InventoryFile(self.file_name_test_hosts_add_host).add_host(
-            "test_group",
-            "test01",
-            "192.168.1.10",
-            "testuser",
-            "testuserpass",
-            "testuserpass"
-        )
+        with tempfile.TemporaryDirectory() as td:
+            os.chdir(td)
+            TestFile.create_blank_file(self.file_name_test_hosts_add_host)
+            InventoryFile(self.file_name_test_hosts_add_host).add_host(
+                "test_group",
+                "test01",
+                "192.168.1.10",
+                "testuser",
+                "testuserpass",
+                "testuserpass"
+            )
 
-        self.assertTrue(os.path.exists(self.file_name_test_hosts_add_host))
-        with open(self.file_name_test_hosts_add_host, 'r') as f:
-            result = f.readlines()
-            self.logger.debug(result)
+            self.assertTrue(os.path.exists(self.file_name_test_hosts_add_host))
+            with open(self.file_name_test_hosts_add_host, 'r') as f:
+                result = f.readlines()
+                self.logger.debug(result)
 
-        self.assertEqual(result[0], "[test_group]\n")
-        self.assertEqual(
-            result[1],
-            "test01"
-            " ansible_connection=ssh"
-            " ansible_host=192.168.1.10"
-            " ansible_port=22"
-            " ansible_user=testuser"
-            " ansible_password=testuserpass"
-            " ansible_become_password=testuserpass\n"
-        )
+            self.assertEqual(result[0], "[test_group]\n")
+            self.assertEqual(
+                result[1],
+                "test01"
+                " ansible_connection=ssh"
+                " ansible_host=192.168.1.10"
+                " ansible_port=22"
+                " ansible_user=testuser"
+                " ansible_password=testuserpass"
+                " ansible_become_password=testuserpass\n"
+            )
 
     def test_delete_host(self):
         """
@@ -91,56 +94,60 @@ class TestInventory(unittest.TestCase):
         """
         Read the data in the hosts file.
         """
-        TestFile.create_blank_file(self.file_name_test_hosts_read_hosts)
-        with open(self.file_name_test_hosts_read_hosts, 'w') as f:
-            f.write('\n'.join([
-                "[test_group]",
-                "test01"
-                " ansible_host=192.168.1.10"
-                " ansible_user=testuser"
-                " ansible_password=testuserpass"
-                " ansible_become_password=testuserpass"
-                " ansible_port=22"
-            ]))
+        with tempfile.TemporaryDirectory() as td:
+            os.chdir(td)
+            TestFile.create_blank_file(self.file_name_test_hosts_read_hosts)
+            with open(self.file_name_test_hosts_read_hosts, 'w') as f:
+                f.write('\n'.join([
+                    "[test_group]",
+                    "test01"
+                    " ansible_host=192.168.1.10"
+                    " ansible_user=testuser"
+                    " ansible_password=testuserpass"
+                    " ansible_become_password=testuserpass"
+                    " ansible_port=22"
+                ]))
 
-        result = InventoryFile(self.file_name_test_hosts_read_hosts)
-        result.read_hosts()
+            result = InventoryFile(self.file_name_test_hosts_read_hosts)
+            result.read_hosts()
 
-        self.assertTrue("test_group" in result.hosts_data)
-        self.assertEqual(result.hosts_data['test_group'].hosts[0].host_name, "test01")
-        self.assertEqual(result.hosts_data['test_group'].hosts[0].option_ansible_host, "192.168.1.10")
-        self.assertEqual(result.hosts_data['test_group'].hosts[0].option_ansible_user, "testuser")
-        self.assertEqual(result.hosts_data['test_group'].hosts[0].option_ansible_password, "testuserpass")
-        self.assertEqual(result.hosts_data['test_group'].hosts[0].option_ansible_become_password, "testuserpass")
-        self.assertEqual(result.hosts_data['test_group'].hosts[0].option_ansible_port, "22")
+            self.assertTrue("test_group" in result.hosts_data)
+            self.assertEqual(result.hosts_data['test_group'].hosts[0].host_name, "test01")
+            self.assertEqual(result.hosts_data['test_group'].hosts[0].option_ansible_host, "192.168.1.10")
+            self.assertEqual(result.hosts_data['test_group'].hosts[0].option_ansible_user, "testuser")
+            self.assertEqual(result.hosts_data['test_group'].hosts[0].option_ansible_password, "testuserpass")
+            self.assertEqual(result.hosts_data['test_group'].hosts[0].option_ansible_become_password, "testuserpass")
+            self.assertEqual(result.hosts_data['test_group'].hosts[0].option_ansible_port, "22")
 
     def test_read_hosts_with_two_hosts(self):
         """
         Read the data in the hosts file.
         """
-        TestFile.create_blank_file(self.file_name_test_hosts_read_hosts_with_two_hosts)
-        with open(self.file_name_test_hosts_read_hosts_with_two_hosts, 'w') as f:
-            for line in ["[test_group]\n",
-                         "test01 ansible_host=192.168.1.10 ansible_user=testuser ansible_password=testuserpass ansible_become_password=testuserpass ansible_port=22\n",
-                         "[test_group2]\n",
-                         "test02 ansible_host=192.168.1.11 ansible_user=testuser2 ansible_password=testuserpass2 ansible_become_password=testuserpass2 ansible_port=23"]:
-                f.write(line)
+        with tempfile.TemporaryDirectory() as td:
+            os.chdir(td)
+            TestFile.create_blank_file(self.file_name_test_hosts_read_hosts_with_two_hosts)
+            with open(self.file_name_test_hosts_read_hosts_with_two_hosts, 'w') as f:
+                for line in ["[test_group]\n",
+                             "test01 ansible_host=192.168.1.10 ansible_user=testuser ansible_password=testuserpass ansible_become_password=testuserpass ansible_port=22\n",
+                             "[test_group2]\n",
+                             "test02 ansible_host=192.168.1.11 ansible_user=testuser2 ansible_password=testuserpass2 ansible_become_password=testuserpass2 ansible_port=23"]:
+                    f.write(line)
 
-        result = InventoryFile(self.file_name_test_hosts_read_hosts_with_two_hosts)
-        result.read_hosts()
+            result = InventoryFile(self.file_name_test_hosts_read_hosts_with_two_hosts)
+            result.read_hosts()
 
-        self.assertTrue("test_group" in result.hosts_data)
-        self.assertEqual(result.hosts_data['test_group'].hosts[0].host_name, "test01")
-        self.assertEqual(result.hosts_data['test_group'].hosts[0].option_ansible_host, "192.168.1.10")
-        self.assertEqual(result.hosts_data['test_group'].hosts[0].option_ansible_user, "testuser")
-        self.assertEqual(result.hosts_data['test_group'].hosts[0].option_ansible_password, "testuserpass")
-        self.assertEqual(result.hosts_data['test_group'].hosts[0].option_ansible_become_password, "testuserpass")
-        self.assertEqual(result.hosts_data['test_group'].hosts[0].option_ansible_port, "22")
+            self.assertTrue("test_group" in result.hosts_data)
+            self.assertEqual(result.hosts_data['test_group'].hosts[0].host_name, "test01")
+            self.assertEqual(result.hosts_data['test_group'].hosts[0].option_ansible_host, "192.168.1.10")
+            self.assertEqual(result.hosts_data['test_group'].hosts[0].option_ansible_user, "testuser")
+            self.assertEqual(result.hosts_data['test_group'].hosts[0].option_ansible_password, "testuserpass")
+            self.assertEqual(result.hosts_data['test_group'].hosts[0].option_ansible_become_password, "testuserpass")
+            self.assertEqual(result.hosts_data['test_group'].hosts[0].option_ansible_port, "22")
 
-        self.assertTrue("test_group2" in result.hosts_data)
-        self.assertEqual(result.hosts_data['test_group2'].hosts[0].host_name, "test02")
-        self.assertEqual(result.hosts_data['test_group2'].hosts[0].option_ansible_host, "192.168.1.11")
-        self.assertEqual(result.hosts_data['test_group2'].hosts[0].option_ansible_user, "testuser2")
-        self.assertEqual(result.hosts_data['test_group2'].hosts[0].option_ansible_password, "testuserpass2")
-        self.assertEqual(result.hosts_data['test_group2'].hosts[0].option_ansible_become_password, "testuserpass2")
-        self.assertEqual(result.hosts_data['test_group2'].hosts[0].option_ansible_port, "23")
+            self.assertTrue("test_group2" in result.hosts_data)
+            self.assertEqual(result.hosts_data['test_group2'].hosts[0].host_name, "test02")
+            self.assertEqual(result.hosts_data['test_group2'].hosts[0].option_ansible_host, "192.168.1.11")
+            self.assertEqual(result.hosts_data['test_group2'].hosts[0].option_ansible_user, "testuser2")
+            self.assertEqual(result.hosts_data['test_group2'].hosts[0].option_ansible_password, "testuserpass2")
+            self.assertEqual(result.hosts_data['test_group2'].hosts[0].option_ansible_become_password, "testuserpass2")
+            self.assertEqual(result.hosts_data['test_group2'].hosts[0].option_ansible_port, "23")
--- test/adc/models/test_setting.py
+++ test/adc/models/test_setting.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+import tempfile
 
 from adc.models.setting import ProjectConfig
 from test.util.file import TestFile
@@ -10,18 +11,14 @@ class TestSetting(unittest.TestCase):
     _directory_name_test_project = 'test_project'
     _project_config_directory = '.ansible_project'
 
-    def setUp(self) -> None:
-        TestFile.delete_directory(self._directory_name_test_project)
-        TestFile.create_blank_directory(self._directory_name_test_project)
-
-    def tearDown(self) -> None:
-        TestFile.delete_directory(self._directory_name_test_project)
-
     def test_project_config_generate(self):
-        ProjectConfig.generate(self._directory_name_test_project)
-        os.chdir(self._directory_name_test_project)
-        ProjectConfig.load()
+        with tempfile.TemporaryDirectory() as td:
+            os.chdir(td)
+            TestFile.create_blank_directory(self._directory_name_test_project)
+            ProjectConfig.generate(self._directory_name_test_project)
+            os.chdir(self._directory_name_test_project)
+            ProjectConfig.load()
 
-        self.assertEqual('test_project', ProjectConfig.get(ProjectConfig.DICT_KEY_PROJECT_NAME))
-        self.assertEqual('/etc/ansible/hosts', ProjectConfig.get(ProjectConfig.DICT_KEY_GLOBAL_INVENTORY_FILE_PATH))
-        self.assertEqual('./hosts', ProjectConfig.get(ProjectConfig.DICT_KEY_PROJECT_INVENTORY_FILE_PATH))
+            self.assertEqual('test_project', ProjectConfig.get(ProjectConfig.DICT_KEY_PROJECT_NAME))
+            self.assertEqual('/etc/ansible/hosts', ProjectConfig.get(ProjectConfig.DICT_KEY_GLOBAL_INVENTORY_FILE_PATH))
+            self.assertEqual('./hosts', ProjectConfig.get(ProjectConfig.DICT_KEY_PROJECT_INVENTORY_FILE_PATH))
