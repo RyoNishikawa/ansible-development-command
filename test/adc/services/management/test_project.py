c8b070248cc615225d9ec26c30baa149ef6d1b02
Ryo Nishikawa <ryo.pg.se@gmail.com>
2019-11-08 19:38:29 +0900

    Modified the script and test.

--- test/adc/services/management/test_project.py
+++ test/adc/services/management/test_project.py
@@ -1,6 +1,7 @@
 import unittest
 import os
 import json
+import tempfile
 
 from adc.services.management.project import Project
 from test.util.file import TestFile
@@ -8,17 +9,24 @@ from test.util.file import TestFile
 
 class TestProject(unittest.TestCase):
 
-    def setUp(self):
-        self.delete_test_files()
+    file_name_test_create_project = 'test_create_project'
+    file_name_test_create_roles = 'test_create_roles'
+    file_name_test_create_project_without_argument = 'test_create_project_without_argument'
 
-    def tearDown(self):
-        self.delete_test_files()
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.delete_test_files()
 
-    def delete_test_files(self):
+    @classmethod
+    def tearDownClass(cls) -> None:
+        pass
+
+    @classmethod
+    def delete_test_files(cls):
         test_dirs = [
-            'test_project1',
-            'test_project2',
-            'test_project3'
+            cls.file_name_test_create_project,
+            cls.file_name_test_create_roles,
+            cls.file_name_test_create_project_without_argument
         ]
         TestFile.delete_directories(test_dirs)
 
@@ -27,55 +35,62 @@ class TestProject(unittest.TestCase):
         This command should be create the project directory.
         And then, create hosts and site.yml in project directory.
         """
-        test_instance = Project('test_project1')
-        ref = test_instance.create_project()
+        with tempfile.TemporaryDirectory() as td:
+            os.chdir(td)
+            test_instance = Project(self.file_name_test_create_project)
+            ref = test_instance.create_project()
 
-        self.assertTrue(os.path.exists('test_project1'))
-        self.assertTrue(os.path.exists('test_project1/hosts'))
-        self.assertTrue(os.path.exists('test_project1/site.yml'))
-        self.assertTrue(os.path.exists('test_project1/.ansible_project/project.json'))
-        with open('test_project1/.ansible_project/project.json', 'r+') as f:
-            project_conf = json.load(f)
-            project_name = project_conf.get("project_name")
-            self.assertEqual(project_name, 'test_project1')
-        self.assertEqual(ref, 0)
+            self.assertTrue(os.path.exists(self.file_name_test_create_project))
+            self.assertTrue(os.path.exists('{0}/hosts'.format(self.file_name_test_create_project)))
+            self.assertTrue(os.path.exists('{0}/site.yml'.format(self.file_name_test_create_project)))
+            self.assertTrue(os.path.exists('{0}/.ansible_project/project.json'.format(self.file_name_test_create_project)))
+            with open('{0}/.ansible_project/project.json'.format(self.file_name_test_create_project), 'r+') as f:
+                project_conf = json.load(f)
+                project_name = project_conf.get("project_name")
+                self.assertEqual(project_name, self.file_name_test_create_project)
+            self.assertEqual(ref, 0)
 
     def test_create_roles(self):
         """
         Create directories and files that need the roles.
         """
-        setup_instance = Project('test_project2')
-        setup_instance.create_project()
+        with tempfile.TemporaryDirectory() as td:
+            os.chdir(td)
+            setup_instance = Project(self.file_name_test_create_roles)
+            setup_instance.create_project()
 
-        os.chdir('test_project2')
-        test_instance = Project()
-        ref = test_instance.create_role('test_role')
+            print(os.getcwd())
+            os.chdir(self.file_name_test_create_roles)
+            test_instance = Project()
+            ref = test_instance.create_role('test_role')
 
-        os.chdir("..")
+            os.chdir("..")
 
-        self.assertTrue(os.path.exists('test_project2/roles'))
-        self.assertTrue(os.path.exists('test_project2/roles/test_role'))
-        self.assertTrue(os.path.exists('test_project2/roles/test_role/tasks/main.yml'))
-        self.assertTrue(os.path.exists('test_project2/roles/test_role/vars/main.yml'))
-        self.assertEqual(ref, 0)
+            self.assertTrue(os.path.exists('{0}/roles'.format(self.file_name_test_create_roles)))
+            self.assertTrue(os.path.exists('{0}/roles/test_role'.format(self.file_name_test_create_roles)))
+            self.assertTrue(os.path.exists('{0}/roles/test_role/tasks/main.yml'.format(self.file_name_test_create_roles)))
+            self.assertTrue(os.path.exists('{0}/roles/test_role/vars/main.yml'.format(self.file_name_test_create_roles)))
+            self.assertEqual(ref, 0)
 
     def test_create_project_without_argument(self):
         """
         This commad should be create the project files when execute it without arguments.
         """
-        os.mkdir('test_project3')
-        os.chdir('test_project3')
+        with tempfile.TemporaryDirectory() as td:
+            os.chdir(td)
+            os.mkdir(self.file_name_test_create_project_without_argument)
+            os.chdir(self.file_name_test_create_project_without_argument)
 
-        setup_instance = Project('.')
-        ref = setup_instance.create_project()
+            setup_instance = Project('.')
+            ref = setup_instance.create_project()
 
-        os.chdir('..')
+            os.chdir('..')
 
-        self.assertTrue(os.path.exists('test_project3/hosts'))
-        self.assertTrue(os.path.exists('test_project3/site.yml'))
-        self.assertTrue(os.path.exists('test_project3/.ansible_project/project.json'))
-        with open('test_project3/.ansible_project/project.json', 'r+') as f:
-            project_conf = json.load(f)
-            project_name = project_conf.get("project_name")
-            self.assertEqual(project_name, 'test_project3')
-        self.assertEqual(ref, 0)
+            self.assertTrue(os.path.exists('{0}/hosts'.format(self.file_name_test_create_project_without_argument)))
+            self.assertTrue(os.path.exists('{0}/site.yml'.format(self.file_name_test_create_project_without_argument)))
+            self.assertTrue(os.path.exists('{0}/.ansible_project/project.json'.format(self.file_name_test_create_project_without_argument)))
+            with open('{0}/.ansible_project/project.json'.format(self.file_name_test_create_project_without_argument), 'r+') as f:
+                project_conf = json.load(f)
+                project_name = project_conf.get("project_name")
+                self.assertEqual(project_name, self.file_name_test_create_project_without_argument)
+            self.assertEqual(ref, 0)
